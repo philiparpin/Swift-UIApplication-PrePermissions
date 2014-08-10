c05c03eb014bc489dd47e739947d829630b8df9e
Jack Rostron <jack@rostron.org.uk>
2014-08-10 19:36:59 +0100

    Added contacts, location, photos, camera callbacks

--- JGRApplicationPermissions.swift
+++ JGRApplicationPermissions.swift
@@ -9,6 +9,10 @@
 import Foundation
 import UIKit
 import EventKit
+import AddressBook
+import CoreLocation
+import AVFoundation
+import AssetsLibrary
 
 //Enums
 enum JGRApplicationPermissionCallback {
@@ -25,7 +29,7 @@ private enum JGRApplicationSystemAccessState {
     case Unknown
 }
 
-class JGRApplicationPermissions: NSObject {
+class JGRApplicationPermissions: NSObject, CLLocationManagerDelegate {
     
     //Singleton
     class var sharedInstance: JGRApplicationPermissions {
@@ -43,16 +47,25 @@ class JGRApplicationPermissions: NSObject {
     
     //Properties
     private var calendarCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
+    private var cameraCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
+    private var contactsCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
+    private var locationCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
+    private var photosCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
     private var remindersCallback: ((state: JGRApplicationPermissionCallback) -> Void)?
+    
+    private var locationManager = CLLocationManager()
     
     //Private Methods
     private func configureAlertWithTitle(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionCallback:(state: JGRApplicationPermissionCallback) -> Void, grantActionClosure:() -> Void) -> UIAlertController {
         
-        let denyAction = UIAlertAction(title: denyButtonTitle, style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
-            completionCallback(state: JGRApplicationPermissionCallback.NoAction)
+        var alertDeny = (denyButtonTitle != nil) ? denyButtonTitle : "Not Now"
+        var alertGrant = (grantButtonTitle != nil) ? grantButtonTitle : "Give Access"
+        
+        let denyAction = UIAlertAction(title: alertDeny, style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
+            completionCallback(state: JGRApplicationPermissionCallback.Later)
         }
 
-        let grantAction = UIAlertAction(title: grantButtonTitle, style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+        let grantAction = UIAlertAction(title: alertGrant, style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
             grantActionClosure()
         }
         
@@ -62,6 +75,12 @@ class JGRApplicationPermissions: NSObject {
         
         return alertView;
     }
+    
+    private func configureAndShowAlertWithTitle(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionCallback:(state: JGRApplicationPermissionCallback) -> Void, grantActionClosure:() -> Void) {
+        let alertView = self.configureAlertWithTitle(title, message: message, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionCallback, grantActionClosure: grantActionClosure);
+        UIApplication.sharedApplication().keyWindow.rootViewController .presentViewController(alertView, animated: true, completion: nil);
+    }
+
     
     //Alerts
     func showCalendarPermissions(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionClosure: (state: JGRApplicationPermissionCallback) -> Void) {
@@ -76,16 +95,97 @@ class JGRApplicationPermissions: NSObject {
         default:
             var alertTitle = (title != nil) ? title : "Access Calendar?";
             var alertBody = (message != nil) ? message : "The current app is requesting access to your Calendar. Do you want to provide it access?"
-            var alertDeny = (denyButtonTitle != nil) ? denyButtonTitle : "Not Now"
-            var alertGrant = (grantButtonTitle != nil) ? grantButtonTitle : "Give Access"
             
             calendarCallback = completionClosure;
             
-            let calendarAlertView = configureAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: alertDeny, grantButtonTitle: alertGrant, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
                 self.requestSystemPermissionForCalendar()
             });
+        }
+    }
+    
+    func showCameraPermissions(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionClosure: (state: JGRApplicationPermissionCallback) -> Void) {
+        
+        switch hasCameraAccess() {
+        case .Granted:
+            completionClosure(state: .NoAction)
+        case .Denied:
+            completionClosure(state: .Denied)
+        case .Restricted:
+            completionClosure(state: .Denied)
+        default:
+            var alertTitle = (title != nil) ? title : "Access Camera?";
+            var alertBody = (message != nil) ? message : "The current app is requesting access to your Camera. Do you want to provide it access?"
             
-            UIApplication.sharedApplication().keyWindow.rootViewController .presentViewController(calendarAlertView, animated: true, completion: nil);
+            cameraCallback = completionClosure;
+            
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+                self.requestSystemPermissionForCamera()
+            });
+        }
+    }
+    
+    func showContactsPermissions(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionClosure: (state: JGRApplicationPermissionCallback) -> Void) {
+        
+        switch hasContactsAccess() {
+        case .Granted:
+            completionClosure(state: .NoAction)
+        case .Denied:
+            completionClosure(state: .Denied)
+        case .Restricted:
+            completionClosure(state: .Denied)
+        default:
+            var alertTitle = (title != nil) ? title : "Access Contacts?";
+            var alertBody = (message != nil) ? message : "The current app is requesting access to your Contacts. Do you want to provide it access?"
+            
+            contactsCallback = completionClosure;
+            
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+                self.requestSystemPermissionForContacts()
+            });
+        }
+    }
+    
+    func showLocationPermissions(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionClosure: (state: JGRApplicationPermissionCallback) -> Void) {
+        
+        switch hasLocationAccess() {
+        case .Granted:
+            completionClosure(state: .NoAction)
+        case .Denied:
+            completionClosure(state: .Denied)
+        case .Restricted:
+            completionClosure(state: .Denied)
+        default:
+            var alertTitle = (title != nil) ? title : "Access Location?";
+            var alertBody = (message != nil) ? message : "The current app is requesting access to your Location. Do you want to provide it access?"
+            
+            locationCallback = completionClosure;
+            
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+                self.locationManager.delegate = self;
+                self.locationManager.requestAlwaysAuthorization()
+            });
+        }
+    }
+    
+    func showPhotosPermissions(title: String?, message: String?, denyButtonTitle: String?, grantButtonTitle: String?, completionClosure: (state: JGRApplicationPermissionCallback) -> Void) {
+        
+        switch hasPhotosAccess() {
+        case .Granted:
+            completionClosure(state: .NoAction)
+        case .Denied:
+            completionClosure(state: .Denied)
+        case .Restricted:
+            completionClosure(state: .Denied)
+        default:
+            var alertTitle = (title != nil) ? title : "Access Photos?";
+            var alertBody = (message != nil) ? message : "The current app is requesting access to your Photos. Do you want to provide it access?"
+            
+            photosCallback = completionClosure;
+            
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+                self.requestSystemPermissionForPhotos()
+            });
         }
     }
     
@@ -101,22 +201,72 @@ class JGRApplicationPermissions: NSObject {
         default:
             var alertTitle = (title != nil) ? title : "Access Reminders?";
             var alertBody = (message != nil) ? message : "The current app is requesting access to your Reminders. Do you want to provide it access?"
-            var alertDeny = (denyButtonTitle != nil) ? denyButtonTitle : "Not Now"
-            var alertGrant = (grantButtonTitle != nil) ? grantButtonTitle : "Give Access"
             
             remindersCallback = completionClosure;
             
-            let calendarAlertView = configureAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: alertDeny, grantButtonTitle: alertGrant, completionCallback: completionClosure, grantActionClosure: { () -> Void in
-                self.requestSystemPermissionForCalendar()
+            configureAndShowAlertWithTitle(alertTitle, message: alertBody, denyButtonTitle: denyButtonTitle, grantButtonTitle: grantButtonTitle, completionCallback: completionClosure, grantActionClosure: { () -> Void in
+                self.requestSystemPermissionForReminders()
             });
-            
-            UIApplication.sharedApplication().keyWindow.rootViewController .presentViewController(calendarAlertView, animated: true, completion: nil);
         }
     }
     
     //Permission Checks
     private func hasCalendarAccess() -> JGRApplicationSystemAccessState {
         switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
+        case .Authorized:
+            return JGRApplicationSystemAccessState.Granted
+        case .Denied:
+            return JGRApplicationSystemAccessState.Denied
+        case .Restricted:
+            return JGRApplicationSystemAccessState.Restricted
+        case .NotDetermined:
+            return JGRApplicationSystemAccessState.Unknown
+        }
+    }
+    
+    private func hasCameraAccess() -> JGRApplicationSystemAccessState {
+        switch AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo) {
+        case .Authorized:
+            return JGRApplicationSystemAccessState.Granted
+        case .Denied:
+            return JGRApplicationSystemAccessState.Denied
+        case .Restricted:
+            return JGRApplicationSystemAccessState.Restricted
+        case .NotDetermined:
+            return JGRApplicationSystemAccessState.Unknown
+        }
+    }
+    
+    private func hasContactsAccess() -> JGRApplicationSystemAccessState {
+        switch ABAddressBookGetAuthorizationStatus() {
+        case .Authorized:
+            return JGRApplicationSystemAccessState.Granted
+        case .Denied:
+            return JGRApplicationSystemAccessState.Denied
+        case .Restricted:
+            return JGRApplicationSystemAccessState.Restricted
+        case .NotDetermined:
+            return JGRApplicationSystemAccessState.Unknown
+        }
+    }
+    
+    private func hasLocationAccess() -> JGRApplicationSystemAccessState {
+        switch CLLocationManager.authorizationStatus() {
+        case .Authorized:
+            return JGRApplicationSystemAccessState.Granted
+        case .AuthorizedWhenInUse:
+            return JGRApplicationSystemAccessState.Granted
+        case .Denied:
+            return JGRApplicationSystemAccessState.Denied
+        case .Restricted:
+            return JGRApplicationSystemAccessState.Restricted
+        case .NotDetermined:
+            return JGRApplicationSystemAccessState.Unknown
+        }
+    }
+    
+    private func hasPhotosAccess() -> JGRApplicationSystemAccessState {
+        switch ALAssetsLibrary.authorizationStatus() {
         case .Authorized:
             return JGRApplicationSystemAccessState.Granted
         case .Denied:
@@ -149,11 +299,40 @@ class JGRApplicationPermissions: NSObject {
         })
     }
     
+    private func requestSystemPermissionForCamera() { //Camera
+        AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: {(granted: Bool) -> Void in
+            (granted) ? self.cameraCallback!(state: JGRApplicationPermissionCallback.Granted) : self.cameraCallback!(state: JGRApplicationPermissionCallback.Denied)
+        })
+    }
+    
+    private func requestSystemPermissionForContacts() { //Contacts
+        var emptyDictionary: CFDictionaryRef?
+        var addressBook = !ABAddressBookCreateWithOptions(emptyDictionary, nil)
+        ABAddressBookRequestAccessWithCompletion(addressBook, {(success :Bool, error :CFError!) in
+            (success) ? self.contactsCallback!(state: JGRApplicationPermissionCallback.Granted) : self.contactsCallback!(state: JGRApplicationPermissionCallback.Denied)
+        })
+    }
+    
+    private func requestSystemPermissionForPhotos() { //Photos
+        let assetsLibrary = ALAssetsLibrary()
+        assetsLibrary.enumerateGroupsWithTypes(ALAssetsGroupType(ALAssetsGroupAll), usingBlock: {(group: ALAssetsGroup!, stop: UnsafeMutablePointer<ObjCBool>) in
+            self.photosCallback!(state: JGRApplicationPermissionCallback.Granted)
+            }, failureBlock: {(error: NSError!) in
+                self.photosCallback!(state: JGRApplicationPermissionCallback.Denied)
+        });
+    }
+    
     private func requestSystemPermissionForReminders() { //Reminders
         let eventStore = EKEventStore();
         eventStore.requestAccessToEntityType(EKEntityTypeReminder, completion: {(granted: Bool, error: NSError!) in
             (granted) ? self.remindersCallback!(state: JGRApplicationPermissionCallback.Granted) : self.remindersCallback!(state: JGRApplicationPermissionCallback.Denied)
         })
+    }
+    
+    //Location Manager Delegate
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        self.locationManager.stopUpdatingLocation()
+        (status == CLAuthorizationStatus.Authorized) ? self.contactsCallback!(state: JGRApplicationPermissionCallback.Granted) : self.contactsCallback!(state: JGRApplicationPermissionCallback.Denied)
     }
     
     
