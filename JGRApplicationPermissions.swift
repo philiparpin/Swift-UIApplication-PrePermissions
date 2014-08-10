c7030edb923df73a78992e3deaae31e18a827ba1
Jack Rostron <jack@rostron.org.uk>
2014-08-10 15:24:10 +0100

    Added JGRApplicationPermissions.swift

---
+++ JGRApplicationPermissions.swift
@@ -0,0 +1,9 @@
+//
+//  JGRApplicationPermissions.swift
+//  PrePermissions
+//
+//  Created by Jack Rostron on 10/08/2014.
+//  Copyright (c) 2014 Jack Rostron. All rights reserved.
+//
+
+import Foundation
